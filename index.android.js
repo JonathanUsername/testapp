86ed62db5da71332e51220e566be2a99cecd1e8e
Jonathan King <jonathan.j.king@gmail.com>
2017-02-26 16:45:46 +0000

    Fix lint

--- index.android.js
+++ index.android.js
@@ -9,81 +9,114 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
+  TouchableHighlight,
   DeviceEventEmitter,
   View
 } from 'react-native';
 import { SensorManager } from 'NativeModules';
 
-function float2 (f) {
-    return parseFloat(f.toFixed(2));
-}
-
-function roundCoordinates(data) {
-    return {
-        x: float2(data.x),
-        y: float2(data.y),
-        z: float2(data.z)
-    };
-}
+const MAX_ACCELERATION = 11.0;
 
 export default class testapp extends Component {
     state = {
+        sensing: false,
         x: 100,
         y: 50,
         z: 0
     }
 
-    componentDidMount() {
-         // Start the accelerometer with a minimum delay of 100ms between events.
-        SensorManager.startAccelerometer(100);
-        DeviceEventEmitter.addListener('Accelerometer', (data) => {
-            const {x ,y ,z} = roundCoordinates(data);
-            this.setState({x, y, z});
-        });
+    checkAcceleration = (data) => {
+        const {x, y, z} = data;
+        const oldState = [
+            this.state.x,
+            this.state.y,
+            this.state.z
+        ];
+        const highValues = oldState.filter(i => i > MAX_ACCELERATION);
+
+        if (highValues.length > 0) {
+
+        }
+        return {
+            x: float2(data.x),
+            y: float2(data.y),
+            z: float2(data.z)
+        };
     }
 
-    componentWillUnmount() {
-        SensorManager.stopAccelerometer();
-    }
+    toggleSensor = () => {
+        const {sensing} = this.state;
+        if (!sensing) {
+            // Start the accelerometer with a minimum delay of 100ms between events.
+            SensorManager.startAccelerometer(100);
+            DeviceEventEmitter.addListener('Accelerometer', (data) => {
+                const {x ,y ,z, tooMuch} = this.checkAcceleration(data);
+                this.setState({x, y, z, tooMuch});
+            });
+            this.setState({sensing: true});
+        } else {
+            SensorManager.stopAccelerometer();
+            this.setState({sensing: false});
+        }
+    };
 
     render() {
-        const {x, y, z} = this.state;
+        const {sensing, x, y, z} = this.state;
         return (
             <View style={styles.container}>
                 <Text style={styles.welcome}>
                     Hello this is a test
                 </Text>
-                <Text style={styles.welcome}>
-                    {x}
-                </Text>
-                <Text style={styles.welcome}>
-                    {y}
-                </Text>
-                <Text style={styles.welcome}>
-                    {z}
-                </Text>
+                <TouchableHighlight onPress={this.toggleSensor} style={styles.button}>
+                    <Text style={styles.buttonText}>
+                        {sensing ? 'Stop' : 'Start'}
+                    </Text>
+                </TouchableHighlight>
+                {sensing && (
+                    <View>
+                        <Text style={styles.welcome}>
+                            {x}
+                        </Text>
+                        <Text style={styles.welcome}>
+                            {y}
+                        </Text>
+                        <Text style={styles.welcome}>
+                            {z}
+                        </Text>
+                    </View>
+                )}
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    button: {
+        margin: 10,
+        padding: 20,
+        backgroundColor: 'green',
+    },
+    buttonText: {
+        textAlign: 'center',
+        fontSize: 25,
+        color: 'white'
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
 
 AppRegistry.registerComponent('testapp', () => testapp);
